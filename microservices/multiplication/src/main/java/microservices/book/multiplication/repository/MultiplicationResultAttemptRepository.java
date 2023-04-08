2387cade1d1ec3272f7e76df290cc94625a0c5e1
massimo.dapa <massimo.dapa96@gmail.com>
2023-04-09 01:02:54 +0200

    bug fix: sql query.

--- microservices/multiplication/src/main/java/microservices/book/multiplication/repository/MultiplicationResultAttemptRepository.java
+++ microservices/multiplication/src/main/java/microservices/book/multiplication/repository/MultiplicationResultAttemptRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface MultiplicationResultAttemptRepository extends CrudRepository<MultiplicationResultAttempt, Long> {
 
-    @Query(value = "SELECT * FROM MULTIPLICATION_ATTEMPT RIGHT JOIN (SELECT * FROM MULT_USER WHERE USER_ALIAS = :userAlias)", nativeQuery = true)
+    @Query(value = "SELECT * FROM MULTIPLICATION_ATTEMPT AS ma WHERE ma.USER_ID = (SELECT USER_ID FROM MULT_USER WHERE USER_ALIAS = :userAlias LIMIT 1) ORDER BY ID DESC LIMIT 5", nativeQuery = true)
     List<MultiplicationResultAttempt> findFirst5ByUserAlias(String userAlias);
 }
