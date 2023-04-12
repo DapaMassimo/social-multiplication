74c3eba50a061803e9b4dfd5c80e07cd5f44be83
massimo.dapa <massimo.dapa96@gmail.com>
2023-04-12 08:10:35 +0200

    added api gateway url.

--- ark/zuul/src/main/java/com/ark/zuul/configuration/WebConfig.java
+++ ark/zuul/src/main/java/com/ark/zuul/configuration/WebConfig.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
